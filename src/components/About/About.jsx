c037aaf7ee764df930dc0e4a25a61efe2470ff61
Aman Singh <amansingh080704@gmail.com>
2024-12-31 23:34:53 +0530

    in the final changes for about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,17 +8,26 @@ const About = () => {
   return (
     <>
 
-
-
-
-
 <div className="about-container">
 <div className="heading">
   <img src="/images/grid.png" alt="Grid" className="grid-image" />
-  <h1>ABOUT US</h1>
+  <h1> ABOUT US </h1>
   <img src="/images/star.png" alt="Star" className="star-left" />
   <img src="/images/star.png" alt="Star" className="star-right" />
 </div>
+
+ {/* New Section with Circle and Text */}
+ <div className="circle-text-container">
+          <div className="circle"></div>
+          </div>
+
+          <div className="text-right">
+            <h2>OUR VISION</h2>
+            <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker.
+            </p>
+          </div>
+        
 
 
   <img
@@ -37,7 +46,7 @@ const About = () => {
 
 
 
-  <div className="about-container">
+  <div className="about-container1">
       {/* Left Leaf */}
       <div className="design-left">
         <img
