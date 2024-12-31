8e44a81a2fd4e3ef894957ceb2c11ab586dcb6a6
AbbasAliNaqvi <abbasalinaqvi888@gmail.com>
2024-12-31 19:28:47 +0530

    Changed Styling & layout

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,67 +1,82 @@
 import React, { useState } from 'react';
 import styles from './Team.module.css';
-import teamData from './teamData';
+import teamData from '../components/teamData';
+import img14 from '../components/Elements/image 14.png';  
+import img10 from '../components/Elements/image 10.png';  
+import img5 from '../components/Elements/image 5.png';
+import img1 from '../components/Elements/img1.png';
 
-function Team() {
+const Team = () => {
   const [selectedTeam, setSelectedTeam] = useState("Core");
 
   return (
-    <div className={styles['page-background']}>
-      <div className={styles['team-container']}>
-   
-        <div className={`${styles['side-splash']} ${styles['splash-left']}`}></div>
-        <div className={`${styles['side-splash']} ${styles['splash-right']}`}></div>
-
-       
+    <div className={styles.pageBackground}>
+      <div className={styles.teamContainer}>
         <img
-          src="/path/to/leaf-image.png"
+          src={img5}
           alt="Decorative Leaf"
-          className={`${styles['leaf-decoration']} ${styles['leaf-top-right']}`}
+          className={`${styles.leafDecoration} ${styles.leafTopRight}`}
         />
         <img
-          src="/path/to/leaf-image.png"
+          src={img5}
           alt="Decorative Leaf"
-          className={`${styles['leaf-decoration']} ${styles['leaf-bottom-left']}`}
+          className={`${styles.leafDecoration} ${styles.leafBottomLeft}`}
         />
 
-        <div className={`${styles['diamond-decoration']} ${styles['diamond-1']}`}></div>
-        <div className={`${styles['diamond-decoration']} ${styles['diamond-2']}`}></div>
+        <div className={`${styles.diamondDecoration} ${styles.diamond1}`}></div>
+        <div className={`${styles.diamondDecoration} ${styles.diamond2}`}></div>
 
-        <header className={styles['team-header']}>
+        <header className={styles.teamHeader}>
           <h1>MEET OUR TEAM</h1>
-          <div className={styles['wavy-line']}></div>
         </header>
 
-        <nav className={styles['category-nav']}>
+        <nav className={styles.categoryNav}>
           {Object.keys(teamData).map((team) => (
             <button
               key={team}
-              className={`${styles['category-button']} ${
-                selectedTeam === team ? styles['active'] : ''
+              className={`${styles.categoryButton} ${
+                selectedTeam === team ? styles.active : ''
               }`}
               onClick={() => setSelectedTeam(team)}
             >
               {team}
             </button>
           ))}
+          <div className={styles.wavyLine}>
+            <img src={img1} alt="Wavy line decoration" />
+          </div>
         </nav>
 
-        <section className={styles['team-grid']}>
+        <section className={styles.teamGrid}>
           {teamData[selectedTeam]?.map((member, index) => (
-            <div key={index} className={styles['team-card']}>
-              <img
-                src={member.img}
-                alt={member.name}
-                className={styles['team-image']}
-              />
-              <p className={styles['member-name']}>{member.name}</p>
-              <p className={styles['member-description']}>{member.description}</p>
+            <div key={`${member.name}-${index}`} className={styles.teamCardContainer}>
+              <div className={styles.teamCardBackground}></div>
+              <div className={styles.teamCard}>
+                <img
+                  src={member.img}
+                  alt={member.name}
+                  className={styles.teamImage}
+                />
+                <p className={styles.memberName}>{member.name}</p>
+                <p className={styles.memberDescription}>{member.description}</p>
+              </div>
             </div>
           ))}
         </section>
       </div>
+
+      <img 
+        src={img14} 
+        alt="Bottom Left Decoration" 
+        className={styles.bottomLeftImage} 
+      />
+      <img 
+        src={img10} 
+        alt="Bottom Right Decoration" 
+        className={styles.bottomRightImage} 
+      />
     </div>
   );
-}
+};
 
 export default Team;
